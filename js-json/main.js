c293540ab9126fe956b455fcde5816d67aeef669
MaxBasharin <maxbasharinweb@gmail.com>
2022-10-16 23:56:10 +0300

    added js-json

--- js-json/main.js
+++ js-json/main.js
@@ -1,28 +1,6 @@
-let namebox = document.getElementById('NameBox')
-let insBtn = document.getElementById('Insbtn')
-let selBtn = document.getElementById('Selbtn')
-let updBtn = document.getElementById('Updbtn')
-let delBtn = document.getElementById('Delbtn')
+var jsonStr = '{"theTeam":[{"teamId":"1","status":"pending"},{"teamId":"2","status":"member"},{"teamId":"3","status":"member"}]}';
 
-function InsertData(){
-	setInterval(ref)
-}
-
-async function getResponse() { 
-	let response = await fetch('https://memony-base-default-rtdb.firebaseio.com/posts.json')
-	let content = await response.json()
-
-
-	let list = document.querySelector('.posts')
-	let key;
-	for (key in content) {
-
-		list.innerHTML += `
-		<li class="post">
-		<h4>${content[key].title}</h4>
-	</li>
-		`
-	}
- }
-
- getResponse()
+var obj = JSON.parse(jsonStr);
+obj['theTeam'].push({ "teamId": "4", "status": "pending" });
+jsonStr = JSON.stringify(obj);
+console.log(obj)
